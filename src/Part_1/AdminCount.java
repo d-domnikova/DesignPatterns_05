e6f3f6662d49a0ac69097937ea9739516ba4f830
chntk <dddomnikova@gmail.com>
2024-05-10 17:38:47 +0300

    [1] Updated part 1

---
+++ src/Part_1/AdminCount.java
@@ -0,0 +1,16 @@
+package Part_1;
+
+public class AdminCount {
+    private int count;
+    public AdminCount() {
+        this.count = 0;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void increaseCount(){
+        count++;
+    }
+}
--- src/Part_1/UserListProcessor.java
+++ src/Part_1/UserListProcessor.java
@@ -2,38 +2,46 @@ package Part_1;
 import java.util.ArrayList;
 
 public class UserListProcessor {
-    private ArrayList<String> usernames;
-    public UserListProcessor(ArrayList<String> usernames) {
-        this.usernames = usernames;
+    private ArrayList<User> users;
+    public UserListProcessor(ArrayList<User> user) {
+        this.users = user;
     }
     public void processUserList() {
-        // Long method
-        for (int i = 0; i < usernames.size(); i++) {
-            String username = usernames.get(i);
-            System.out.println("Processing user: " + username);
-            // Some processing logic...
-            if (username.contains("admin")) {
-                System.out.println("Admin user detected!");
-            }
+        for (User user : users) {
+            processUser(user);
         }
     }
+    public void processUser(User user){
+        System.out.println("Processing user: " + user.getUsername());
+        if (isAdmin(user)) {
+            System.out.println("Admin user detected!");
+        }
+    }
+
+    public boolean isAdmin(User user){
+        return user.getUsername().contains("admin");
+    }
+
     public int countAdminUsers() {
-        // Primitive obsession
-        int adminCount = 0;
-        for (String username : usernames) {
-            if (username.contains("admin")) {
-                adminCount++;
+        AdminCount adminCount = new AdminCount();
+        for (User user : users){
+            if (isAdmin(user)){
+                adminCount.increaseCount();
             }
         }
-        return adminCount;
+        return adminCount.getCount();
     }
-    // Long parameter list
-    public void addUser(String username, String email, String password, String
-            address) {
-        // Adding user...
+
+    public void addUser(User user) {
+        users.add(user);
     }
-    // Data clumps
-    public void sendEmailToUser(String username, String email) {
-        // Sending email...
+
+    public String sendEmailToUser(User receiver) {
+        for (User user : users){
+            if (user.getUsername().equals(receiver.getUsername())){
+                return "Email successfully sent to " + receiver.getEmail();
+            }
+        }
+        return "Listed user doesn't found.";
     }
 }
